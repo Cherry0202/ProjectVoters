a88e94cf34d41ae6afaa8d09360f7bef07211bff
Cherry0202 <ex00ia2432@gmail.com>
2019-06-03 14:03:26 +0900

    [update]to-modele

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
-package Voters
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	println("Hello,Gin")
+	// 初期値
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/", func(context *gin.Context) {
+		println("hello,Voters!!")
+	})
+	r.Run() // 0.0.0.0:8080 でサーバーを立てます。
 }
